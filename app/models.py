0ab309c0628b729000cded2a6363de44aed4c610
Randkey <alekseypokryshkin@gmail.com>
2026-04-06 15:06:57 +0300

    changes

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
 from . import db
 
-class Car(db.Model):
-    __tablename__ = 'cars'
+class Flight(db.Model):
+    __tablename__ = 'flights'
     id = db.Column(db.Integer, primary_key=True)
-    model = db.Column(db.String(100))
-    number = db.Column(db.String(20), unique=True)
-    reserved = db.Column(db.Boolean)
+    flight_number = db.Column(db.String(10), unique=True)
+    departure_airport = db.Column(db.String(100))
+    arrival_airport = db.Column(db.String(100))
+    departure_time = db.Column(db.String(10))
+    duration = db.Column(db.String(10))
 
-class Trip(db.Model):
-    __tablename__ = 'trips'
+class Passenger(db.Model):
+    __tablename__ = 'passengers'
     id = db.Column(db.Integer, primary_key=True)
-    phone = db.Column(db.String(20))
-    sms_code = db.Column(db.String(10))
-    car_model = db.Column(db.String(100))
-    car_number = db.Column(db.String(20))
-    fuel_level = db.Column(db.String(10))
+    phone = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(50))
+    miles = db.Column(db.Integer)
